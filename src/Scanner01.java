60b08e92ee0fd72e8a216a51adf9f5efa3fcbfb8
Mbadak <m.badak99@gmail.com>
2023-03-24 15:19:43 +0100

    240323_02

---
+++ src/Scanner01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Scanner01 {
+
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("LÜtfen bir deger giriniz");
+        int a=in.nextInt();
+    }
+
+}
