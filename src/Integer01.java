f059f7cbb1da559b8dc3b8aa72ded4a929eed308
Mbadak <m.badak99@gmail.com>
2023-03-24 15:08:48 +0100

    240323_01

---
+++ src/Integer01.java
@@ -0,0 +1,11 @@
+public class Integer01 {
+    public static void main(String[] args) {
+
+        int num=111;
+        int x=2147483647;
+        int y=x+1000000000;
+        System.out.println(y);
+
+
+    }
+}
